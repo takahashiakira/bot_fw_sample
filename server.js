2439b244d0a21f1d69b2c5a9c9c773cd2a1e6386
akiratakahashi <zombiedeka@gmail.com>
2017-02-09 17:18:37 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http')
+const port = process.env.PORT || 1337;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
