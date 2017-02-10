f5dc45f8e250b2055de0f4c0e5719ea4fc2ccccf
akiratakahashi <zombiedeka@gmail.com>
2017-02-10 12:55:13 +0900

    Add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# MS Bot Framework Sample
+
+
+## Overview
+
+- MS Bot Framework Sample
+
+### Tools
+
+- [MS Bot Framework](https://dev.botframework.com/)
+- [MS BotBuilder](https://github.com/Microsoft/BotBuilder)
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*-----------------------------------------------------------------------------
+/* -----------------------------------------------------------------------------
 This Bot uses the Bot Connector Service but is designed to showcase whats
 possible on Slack using the framework. The demo shows how to create a looping
 menu, use the built-in prompts, send Pictures, send Hero & Thumbnail Cards,
@@ -6,53 +6,52 @@ send Receipts, and use Carousels.
 
 # RUN THE BOT:
 
-    You can run the bot locally using the Bot Framework Emulator but for the best
-    experience you should register a new bot on Slack and bind it to the demo
-    bot. You can then run the bot locally using ngrok found at https://ngrok.com/.
+  You can run the bot locally using the Bot Framework Emulator but for the best
+  experience you should register a new bot on Slack and bind it to the demo
+  bot. You can then run the bot locally using ngrok found at https://ngrok.com/.
 
-    * Install and run ngrok in a console window using "ngrok http 3978".
-    * Create a bot on https://dev.botframework.com and follow the steps to setup
-      a Slack channel.
-    * For the endpoint you setup on dev.botframework.com, copy the https link
-      ngrok setup and set "<ngrok link>/api/messages" as your bots endpoint.
-    * Next you need to configure your bots MICROSOFT_APP_ID, and
-      MICROSOFT_APP_PASSWORD environment variables. If you're running VSCode you
-      can add these variables to your the bots launch.json file. If you're not
-      using VSCode you'll need to setup these variables in a console window.
-      - MICROSOFT_APP_ID: This is the App ID assigned when you created your bot.
-      - MICROSOFT_APP_PASSWORD: This was also assigned when you created your bot.
-    * To use the bot you'll need to click the join link in the portal which will
-      add it as a contact to your Slack account.
-    * To run the bot you can launch it from VSCode or run "node app.js" from a
-      console window.
+  * Install and run ngrok in a console window using "ngrok http 3978".
+  * Create a bot on https://dev.botframework.com and follow the steps to setup
+    a Slack channel.
+  * For the endpoint you setup on dev.botframework.com, copy the https link
+    ngrok setup and set "<ngrok link>/api/messages" as your bots endpoint.
+  * Next you need to configure your bots MICROSOFT_APP_ID, and
+    MICROSOFT_APP_PASSWORD environment variables. If you're running VSCode you
+    can add these variables to your the bots launch.json file. If you're not
+    using VSCode you'll need to setup these variables in a console window.
+    - MICROSOFT_APP_ID: This is the App ID assigned when you created your bot.
+    - MICROSOFT_APP_PASSWORD: This was also assigned when you created your bot.
+  * To use the bot you'll need to click the join link in the portal which will
+    add it as a contact to your Slack account.
+  * To run the bot you can launch it from VSCode or run "node app.js" from a
+    console window.
+----------------------------------------------------------------------------- */
 
------------------------------------------------------------------------------*/
+const restify = require('restify');
+const builder = require('botbuilder');
 
-var restify = require('restify');
-var builder = require('botbuilder');
-
-//=========================================================
+// =========================================================
 // Bot Setup
-//=========================================================
+// =========================================================
 
 // Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+const connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
+const bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 
-//=========================================================
+// =========================================================
 // Activity Events
-//=========================================================
+// =========================================================
 
 bot.on('conversationUpdate', function (message) {
    // Check for group conversations
