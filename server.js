f0ca2c915e3fbc6e27d006a754179791c1d5e720
akiratakahashi <zombiedeka@gmail.com>
2017-02-10 10:37:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const restify = require('restify');
-const builder = require('botbuilder');
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-  console.log('%s listening to %s', server.name, server.url);
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
-const connector = new builder.ChatConnector({
-  appId: process.env.MS_APP_ID,
-  appPassword: process.env.MS_APP_PW
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-const bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-bot.dialog('/', (session) => {
-  session.send('Hello World');
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
 });
